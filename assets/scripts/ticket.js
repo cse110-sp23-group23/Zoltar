4dc0615ecb26c6d110f2b6805813b992f7950ff7
Luke-Sheltraw <lukesheltraw@gmail.com>
2023-05-21 23:06:24 -0700

    changing back

--- assets/scripts/ticket.js
+++ assets/scripts/ticket.js
@@ -67,15 +67,6 @@ window.addEventListener('keydown', (event) => {
 	}
 });
 
-/**
- * Test function
- * @param TEST
- */
-function test() {
-	return 'testing something';
-}
-test();
-
 function init() {
 	ticket = {
 		main: document.querySelector('.ticket-wrapper'),
