1c608fd7992bb3ea706f58556e7838e04dd94359
Luke-Sheltraw <lukesheltraw@gmail.com>
2023-05-28 19:40:03 -0700

    flag off

--- assets/scripts/fortunes.js
+++ assets/scripts/fortunes.js
@@ -1,7 +1,7 @@
 import { state, updateTicket } from './ticket.js';
 
 const ENDPOINT_URL = 'https://zoltar-gpt-api-team-23.netlify.app/.netlify/functions/gptendpoint';
-const ENABLE_GPT_FLAG = true;
+const ENABLE_GPT_FLAG = false;
 
 let data;
 
