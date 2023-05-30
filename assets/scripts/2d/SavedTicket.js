3dc20ed520e3ac598951bbee4be423090ef16b05
MarcBaeuerle <92479171+MarcBaeuerle@users.noreply.github.com>
2023-05-30 16:46:55 -0700

    2d Saved tickets prototype

--- assets/scripts/2d/SavedTicket.js
+++ assets/scripts/2d/SavedTicket.js
@@ -1,52 +1,55 @@
-// import { convertArrToReadableString } from '../ticket.js';
+import { convertArrToReadableString } from '../ticket.js';
 
-class SavedTicket extends HTMLElement {
+/**
+ * SavedTicket2D class for creating custom HTML elements. This class represents a ticket that
+ * the user previously received, and will be displayed in aggregate with the rest of the tickets
+ * to the user. State can be set to control displayed fortune and lucky numbers.
+ * @class SavedTicket2D
+ * @extends HTMLElement
+ */
+class SavedTicket2D extends HTMLElement {
 	constructor() {
 		super();
 
 		const shadow = this.attachShadow({ mode: 'open' });
-		const div = document.createElement('div');
-		div.appendChild(document.createTextNode('Why not show?'));
+		// const div = document.createElement('div');
+		// div.appendChild(document.createTextNode('Why not show?'));
 
-		// let section = document.createElement('section');
-		// this.section.classList.add('ticket');
-		// const style = document.createElement('style');
-		// style.innerHTML = `
-		//     @import url('./assets/styles/twodee.css');
+		const section = document.createElement('section');
+		section.classList.add('ticket', 'savedTicket');
+		const style = document.createElement('style');
+		style.innerHTML = `
+		    @import url('./assets/styles/twodee.css');
+            @import url('./assets/styles/ticket.css');
+		`;
 
-		//     section {
-		//         height: 50px;
-		//         width: 50px;
-		//         background-color: blue;
-		//         z-index: 10000;
-		//         position: absolute;
-		//         left: 50px;
-		//         display: inline;
-		//     }
-		// `;
+		section.innerHTML = '';
 
-		// this.section.innerHTML = `<p>WTF MAN</p>`;
-
-		// shadow.append(style);
-		shadow.append(div);
+		shadow.append(style, section);
 	}
 
-	// set content(state) {
-	//     if (!state) {
-	//         return;
-	//     }
-	//     this.section.innerHTML = `
-	//         <img loading="lazy" src="/assets/images/horizontalrule.png" class="top">
-	//         <span class="closeTicket" id="closeTicket">&times;</span>
-	//         <h1 class="ticket-title" class="loaded-message">YOUR FORTUNE</h1>
-	//         <img loading="lazy" src="assets/images/header.png" class="ticket-header-image">
-	//         <p class="fortune-text">${state.currentMessage}</p>
-	//			<p class="fortune-number">
-	//				Your lucky numbers are ${convertArrToReadableString(state.currentNumbers)}
-	// 			</p>
-	//         <img loading="lazy" src="/assets/images/horizontalrule.png" class="bottom">
-	//         `;
-	// }
+	/**
+	 * Setter for the content of the SavedTicket2D. Give an object with (string)'currentMessage'
+	 * and (number array)'currentNumbers' properties. Updates the ticket content with respective
+	 * properties
+	 * @param { Object } state - an object with currentMessage and currentNumbers
+	 */
+	set content(state) {
+		if (!state) {
+			return;
+		}
+
+		this.shadowRoot.querySelector('section').innerHTML = `
+            <img loading="lazy" src="/assets/images/horizontalrule.png" class="top">
+	        <h1 class="ticket-title" class="loaded-message">YOUR FORTUNE</h1>
+            <img loading="lazy" src="assets/images/header.png" class="ticket-header-image">
+	        <p class="fortune-text">${state.currentMessage}</p>
+			<p class="fortune-number">
+				Your lucky numbers are ${convertArrToReadableString(state.currentNumbers)}
+			</p>
+	        <img loading="lazy" src="/assets/images/horizontalrule.png" class="bottom">
+	        `;
+	}
 }
 
-customElements.define('saved-ticket', SavedTicket);
+customElements.define('saved-ticket', SavedTicket2D);
--- assets/scripts/2d/ticketHistory.js
+++ assets/scripts/2d/ticketHistory.js
@@ -1,13 +1,78 @@
 import { getAllTickets } from '../storage.js';
-// import { convertArrToReadableString } from '../ticket.js';
 
 const historyCircleButton = document.querySelector('#ticketHistoryCircle');
 const ticketHistoryCount = document.querySelector('#ticketHistoryCount');
 const ticketHistoryTickets = document.querySelector('#ticketHistoryTickets');
-// let currentCards = [];
-let count = 0;
+const ticketHistoryBackground = document.querySelector('.ticketHistoryBackground');
+const arrowButtons = document.querySelector('.historyArrowButtons');
 
+const OPEN = 1;
+const CLOSE = 0;
+
+let historyOnScreen = false;
+let count = 0;
+let currentCards = [];
+
+/**
+ * Position cards on screen relative to currently selected card
+ * @param none
+ * STILL WORKING ON THIS -Marc
+ */
+function translateCards() {
+	currentCards.forEach((card, i) => {
+		const distance = i - 1;
+		const geoSumDistance = (distance < 0 ? -1 : 1) * 20 * (1 - 0.9 ** Math.abs(distance));
+		const scaleFactor = 0.9 ** Math.abs(distance);
+		const cardMod = card;
+		cardMod.style.transform = `translate(calc(${geoSumDistance}vw - 50%), -50%) scale(${scaleFactor})`;
+	});
+
+	ticketHistoryTickets.style.left = '50%';
+	ticketHistoryTickets.style.transform = 'translateX(-50%)';
+}
+
+/**
+ * displays the saved tickets.
+ * STILL WORKING ON THIS -Marc
+ */
+function displayStorage() {
+	currentCards.forEach((card) => {
+		ticketHistoryTickets.append(card);
+	});
+
+	translateCards();
+}
+
+/**
+ * Toggles items display properties
+ * @param {boolean} action OPEN to display, CLOSE to hide
+ */
+function toggleItems(action) {
+	const items = [ticketHistoryBackground, arrowButtons];
+
+	items.forEach((tag) => {
+		const asset = tag;
+		if (action) {
+			asset.style.display = 'inline';
+		} else {
+			asset.style.display = 'none';
+		}
+	});
+}
+
+/**
+ * Gets all the tickets from storage and displays them
+ * STILL WORKING ON THIS -Marc
+ */
 historyCircleButton.addEventListener('click', () => {
+	if (historyOnScreen) {
+		ticketHistoryTickets.innerHTML = '';
+		historyOnScreen = false;
+		toggleItems(CLOSE);
+		return;
+	}
+	toggleItems(OPEN);
+	currentCards = [];
 	const allTickets = getAllTickets();
 	count = allTickets.length;
 
@@ -15,15 +80,18 @@ historyCircleButton.addEventListener('click', () => {
 		return;
 	}
 
-	// let state = {
-	// 	currentMessage: allTickets[0].currentMessage,
-	// 	currentNumbers: allTickets[0].currentNumbers,
-	// };
+	allTickets.forEach((ticket) => {
+		const card = document.createElement('saved-ticket');
+		const state = {
+			currentMessage: ticket.currentMessage,
+			currentNumbers: ticket.currentNumbers,
+		};
 
-	const card = document.createElement('saved-ticket');
-	// card.content = state;
-
-	ticketHistoryTickets.append(card);
+		card.content = state;
+		currentCards.push(card);
+	});
+	displayStorage();
+	historyOnScreen = true;
 });
 
 function init() {
