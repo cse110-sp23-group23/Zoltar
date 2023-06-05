17cb6be556b4cc13cddf048c0327d0bf27493573
MarcBaeuerle <92479171+MarcBaeuerle@users.noreply.github.com>
2023-06-04 22:10:22 -0700

    compressed images and added flickering lights

--- assets/scripts/2d/camera.js
+++ assets/scripts/2d/camera.js
@@ -2,11 +2,41 @@ import { isHistoryOnScreen } from './ticketHistory.js';
 import { isTicketOnScreen } from './main.js';
 
 let cameraEl;
+let vignette;
 
 let xValue = 0;
 let yValue = 0;
 let rotateDegree = 0;
 let frame = 0;
+const FLICKER_DELAY = 130;
+const FRAMES_BETWEEN_FLICKER = 400;
+const LIGHTS_ON = 'radial-gradient(circle at center, rgba(0, 0, 0, 0) 20%, rgba(0, 0, 0, 1))';
+const LIGHTS_OFF = 'radial-gradient(circle at center, rgba(0, 0, 0, 0) 20%, rgba(0, 0, 0, 0.7))';
+
+/**
+ * adjusts vignette value to seem like lights flicker
+ * @param none
+ * I live to drink code climates tears
+ */
+function flickerLights() {
+	vignette.style.background = LIGHTS_OFF;
+	setTimeout(() => {
+		vignette.style.background = LIGHTS_ON;
+		setTimeout(() => {
+			vignette.style.background = LIGHTS_OFF;
+			setTimeout(() => {
+				vignette.style.background = LIGHTS_ON;
+				if (Math.random() > 0.5) return;
+				setTimeout(() => {
+					vignette.style.background = LIGHTS_OFF;
+					setTimeout(() => {
+						vignette.style.background = LIGHTS_ON;
+					}, FLICKER_DELAY);
+				}, FLICKER_DELAY);
+			}, FLICKER_DELAY);
+		}, FLICKER_DELAY);
+	}, FLICKER_DELAY);
+}
 
 /**
  * Event Listener that creates a 3D like effect for the 2d Zoltar.
@@ -14,6 +44,7 @@ let frame = 0;
 window.addEventListener('mousemove', (e) => {
 	// Pauses Zoltar's movements if any asset if displayed on screen. Eg. ticket, history
 	frame += 1;
+	if (frame % FRAMES_BETWEEN_FLICKER === 0) flickerLights();
 	if (frame % 2 === 0) return;
 	if (isHistoryOnScreen() || isTicketOnScreen()) return;
 
@@ -34,6 +65,7 @@ window.addEventListener('mousemove', (e) => {
 
 function init() {
 	cameraEl = document.querySelectorAll('.camera');
+	vignette = document.querySelector('.vignette');
 }
 
 document.addEventListener('DOMContentLoaded', init);
