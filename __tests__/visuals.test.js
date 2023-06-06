675d57e38d15c9bf1296a10876a1a9f4dca770b0
Luke-Sheltraw <lukesheltraw@gmail.com>
2023-06-06 16:14:57 -0700

    resolving missing params

--- __tests__/visuals.test.js
+++ __tests__/visuals.test.js
@@ -1,15 +1,24 @@
-/* eslint-disable import/no-extraneous-dependencies */
 /*
  * @jest-environment node
  */
+/* eslint-disable import/no-extraneous-dependencies */
 import percySnapshot from '@percy/puppeteer';
 import puppeeter from 'puppeteer';
+
+beforeAll((done) => {
+	done();
+});
 
 describe('visual testing thru percy.io', () => {
 	it('loads the homepage', async () => {
 		const browser = await puppeeter.launch();
 		const page = await browser.newPage();
-		await page.goto('https://zoltar.live');
+		await page.goto('http://localhost:5500');
 		await percySnapshot(page, 'Basic image');
+		page.close();
 	});
+});
+
+afterAll((done) => {
+	done();
 });
