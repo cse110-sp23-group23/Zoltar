5184d4f2d2c0f87985769855dd5aac9ac38fc198
MarcBaeuerle <92479171+MarcBaeuerle@users.noreply.github.com>
2023-06-06 23:43:06 -0700

    Zoltars eyes move

--- __tests__/visuals.test.js
+++ __tests__/visuals.test.js
@@ -6,10 +6,20 @@ import percySnapshot from '@percy/puppeteer';
 import puppeteer from 'puppeteer';
 
 describe('visual testing thru percy.io', () => {
-	it('loads the homepage', async () => {
+	it('(3D) loads the homepage', async () => {
 		const browser = await puppeteer.launch();
 		const page = await browser.newPage();
 		await page.goto('http://localhost:5500');
+		await percySnapshot(page, 'Loading page image');
+
+		page.close();
+		browser.close();
+	});
+
+	it('(2D) loads the homepage', async () => {
+		const browser = await puppeteer.launch();
+		const page = await browser.newPage();
+		await page.goto('http://localhost:5500/twodee.html');
 		await percySnapshot(page, 'Loading page image');
 
 		page.close();
