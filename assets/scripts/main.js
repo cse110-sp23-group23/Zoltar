ad36bf5e9fa60789a942d5224740481e8426c3bd
Luke-Sheltraw <lukesheltraw@gmail.com>
2023-06-07 20:00:31 -0700

    more refactoring to make main.js smaller

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,5 @@
 import {
 	AmbientLight,
-	BoxGeometry,
 	SphereGeometry,
 	Clock,
 	LoadingManager,
@@ -30,73 +29,10 @@ import { isTicketCurrentlyDisplayed, toggleTicketOn } from './ticket.js';
 import { tellPageLoaded } from './splash.js';
 import { createFortuneOnTicket } from './fortunes.js';
 import { flickerDelay } from './util.js';
+import { options } from './options.js';
 
 // eslint-disable-next-line no-console
 console.log('%cWelcome to %cZoltar%c.live!', '', 'color: red; font-weight: bolder', '');
-
-const options = {
-	camera: {
-		defaultPosition: new Vector3(-4.2, 1.7, -3.5),
-	},
-	model: {
-		position: new Vector3(0, -2, 0),
-		scale: new Vector3(2, 2, 2),
-	},
-	light: {
-		spotlightItensity: 3,
-		spotlightColor: 0xfff5b6,
-		spotlightPos: new Vector3(-8.4, 3.4, -7),
-		spotlightTarget: new Vector3(-2.5, 1, -0.3),
-		spotlightAngle: Math.PI / 20,
-		spotlightDistance: 30,
-		ambientIntensity: 0.03,
-		ambientColor: 0xffffff,
-	},
-	shake: {
-		intensity: new Vector3(0.03, 0.03, 0.03),
-		minDurationMS: 1000,
-	},
-	flicker: {
-		startProbability: 0.005,
-		onInterval: 0.1,
-		timingFunc: () => (Math.floor(Math.random() * 0.07) + 0.07),
-		countFunc: () => (Math.floor(Math.random() * 2) + 2),
-	},
-	cameraSlide: {
-		speed: 4,
-	},
-	ticketHitbox: {
-		geometry: new BoxGeometry(0.2, 0.11, 0.005),
-		position: new Vector3(-2.005, -0.02, -0.75),
-		rotateY: 0.50,
-	},
-	ticketSlide: {
-		speed: 4,
-		initialPosition: new Vector3(-1.82, -0.051, -0.45),
-		finalPosition: new Vector3(-1.945, -0.051, -0.65),
-	},
-	ticketMat: {
-		urlPrefix: 'assets/images/image-bank-back/background-card-',
-		material: new MeshLambertMaterial({ color: 0xe0c9a6 }),
-		geometry: new BoxGeometry(0.1, 0.005, 0.5),
-		rotationY: 0.57,
-	},
-	eyes: {
-		leftEyePos: new Vector3(-1.84, 1.3, 0.065),
-		rightEyePos: new Vector3(-1.945, 1.3, 0.145),
-		color: 0xbb2929,
-		stepSize: 0.05,
-		frames: [ // of form [action, timeUntilNextMS]
-			['remove', 600],
-			['add', 200],
-			['remove', 100],
-			['add', 300],
-			['remove', 100],
-			['add', 1000],
-		],
-		timingFunc: (i) => 2 * i,
-	},
-};
 
 const state = {
 	currentShakeDuration: 0,
