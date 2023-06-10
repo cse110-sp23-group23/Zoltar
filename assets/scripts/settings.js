f1c80a42b4922a2ed15257cbd74fd8abcef2fbe9
MarcBaeuerle <92479171+MarcBaeuerle@users.noreply.github.com>
2023-06-09 17:54:21 -0700

    credits added, instructions semi added

--- assets/scripts/settings.js
+++ assets/scripts/settings.js
@@ -7,6 +7,8 @@ const DEFAULT_SETTINGS = {
 let dom = {};
 let settings = {
 	isVolumeOn: true,
+	isTicketDisplayed: false,
+	ticketOpened: null,
 };
 
 function refreshSettingsMenu() {
@@ -30,12 +32,68 @@ function toggleMute() {
 	settings.isVolumeOn = !settings.isVolumeOn;
 	putSettingsLocalStorage();
 	refreshSettingsMenu();
-}
+} /* toggleMute */
 
+/**
+ * Opens and closes the settings buttons container
+ * @param none
+ */
 function toggleSettingsContainer() {
 	dom.settingsButton.classList.toggle('clicked');
 	dom.subSettingsBtn.classList.toggle('settings-opacity');
 	dom.subSettingsBtn.classList.toggle('settings-slide-in');
+} /* toggleSettingsContainer */
+
+/**
+ * Closes all of the settings Tickets.
+ * How else am I supposed to describe this function
+ * @param none
+ */
+function closeAllSettingsTickets() {
+	const arr = document.querySelectorAll('.settings-ticket');
+	arr.forEach((ticket) => {
+		ticket.classList.add('hidden');
+	});
+	settings.isTicketDisplayed = false;
+	settings.ticketOpened = null;
+} /* closeAllSettingsTickets */
+
+/**
+ * Displays desired settings ticket
+ * @param {*} ticket settings ticket to open
+ */
+function displaySettingsTicket(ticket) {
+	ticket.classList.remove('hidden');
+	settings.isTicketDisplayed = true;
+	settings.ticketOpened = ticket;
+} /* displaySettingsTicket */
+
+/**
+ * Handles opening and closing settings menu tickets
+ * @param {DOM Object} ticket settings ticket to open/close
+ * @returns none
+ */
+function settingsTicketHandler(ticket) {
+	if (!settings.isTicketDisplayed) {
+		displaySettingsTicket(ticket);
+		return;
+	}
+
+	if (ticket === settings.ticketOpened) {
+		closeAllSettingsTickets();
+		return;
+	}
+
+	closeAllSettingsTickets();
+	displaySettingsTicket(ticket);
+} /* settingsTicketHandler */
+
+/**
+ * Reloads the browser window
+ */
+function reload() {
+	// cant call this on its own 'Illegal invocation'
+	window.location.reload();
 }
 
 function init() {
@@ -45,12 +103,27 @@ function init() {
 		volume: document.querySelector('.volume'),
 		exitButton: document.querySelector('.exit-zoltar'),
 		subSettingsBtn: document.querySelector('.settings-menu-settings'),
+		closeTicket: document.querySelectorAll('.close-settings-ticket'),
+		creditsButton: document.querySelector('.credits-button'),
+		creditsTicket: document.querySelector('.credits'),
+		instructionsButton: document.querySelector('.instructions-button'),
+		instructionsTicket: document.querySelector('.instructions'),
 	};
 
 	getSettingsLocalStorage();
 
+	dom.instructionsButton.addEventListener('click', () => {
+		settingsTicketHandler(dom.instructionsTicket);
+	});
+	dom.creditsButton.addEventListener('click', () => {
+		settingsTicketHandler(dom.creditsTicket);
+	});
+	dom.closeTicket.forEach((button) => {
+		button.addEventListener('click', closeAllSettingsTickets);
+	});
+
 	// eslint-disable-next-line no-restricted-globals
-	dom.exitButton.addEventListener('click', location.reload);
+	dom.exitButton.addEventListener('click', reload);
 	dom.settingsButton.addEventListener('click', toggleSettingsContainer);
 	dom.volume.addEventListener('click', toggleMute);
 }
